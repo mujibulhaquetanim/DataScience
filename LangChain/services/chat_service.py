afd0c7f802f6ab7b866adeb59142d34d95a5339f
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-05 00:24:11 +0600

    misspelling of langchain variable fixed + better indentations added

--- LangChain/services/chat_service.py
+++ LangChain/services/chat_service.py
@@ -1,12 +1,12 @@
 from LangChain.services.langchain_client import LangChainClient
 
-
 class ChatService:
-    def __init__(self, lanchain_client: LangChainClient):
-        self.langchain_client = lanchain_client
+    def __init__(self, langchain_client: LangChainClient):
+        self.langchain_client = langchain_client
     
     def generate_response(self, text: str) -> str:
-        
-        messages = [{"role": "system", "content": "You are a helpful doctor. Help the patient "},{"role": "user", "content": text}]
-        
+        messages = [
+            {"role": "system", "content": "You are a helpful doctor. Help the patient."},
+            {"role": "user", "content": text}
+        ]
         return self.langchain_client.get_chat_completion(messages=messages)
